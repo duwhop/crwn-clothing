1ddaafe977da99ab789b8c1397addd298aa59026
duwhop <kjeanpie@scs.howard.edu>
2021-11-07 11:24:17 -0500

    added redux and implemented userReducer and userActions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,7 +50,7 @@ class SignIn extends React.Component{
                     <FormInput name="password" type="password" value={this.state.password} label="password" handleChange={this.handleChange} required />
                     <div className='buttons'>
                         <CustomButton type='submit' >Sign In</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> {' '}Sign In with Google{' '}</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn> {' '}Sign In with Google{' '}</CustomButton>
                     </div>
 
                     
